0b2fc1d34098c8c33305098de5661423eac98ee5
GIDRO32 <pambossable@gmail.com>
2023-01-04 20:26:16 +0200

    Add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner key = new Scanner(System.in);
+        int age;
+        System.out.println("Enter your age:");
+        age = key.nextInt();
+        if(age <= 0)
+        {
+            System.out.println("It is not your age. It is " + age*-1 + " years before your birth");
+        }
+        else if(age > 0 && age <= 6)
+        {
+            System.out.println("Your common activity is daycare");
+        }
+        else if(age > 6 && age <= 17)
+        {
+            System.out.println("Your common activity is school");
+        }
+        else if (age > 17 && age <= 28)
+        {
+            System.out.println("Your common activities are university and work");
+        }
+        else if (age > 28 && age <= 60)
+        {
+            System.out.println("Your common activity is work");
+        }
+        else if(age > 60 && age <= 100)
+        {
+            System.out.println("You are retired");
+        }
+        else
+        {
+            System.out.println("How are you still alive?");
+        }
     }
 }
